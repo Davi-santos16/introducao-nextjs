099727f07bd7f5735df1ed41bd3e1a1b7e35144a
davi <davicastro213@gmail.com>
2025-05-26 22:30:20 -0300

    feat:atividade

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import ProductList from "@/components/ListaProdutos";
+
+
+
+export default function Page() {
+  return (
+    <div>
+      <h2>Rota de Produtos</h2>
+      <ProductList/>
+      
+    </div>
+  );
+}
